3425273f28e87b96a9a82363b6ce00783c03f530
Chris Lewis <Bonsitm@gmail.com>
2015-10-05 16:43:41 -0700

    Fix layout for Chalk

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
+/**
+ * main.c - Sets up the main Window and an ActionMenuLayer to prompt a button
+ * press. Once pressed, an ActionMenu is shown to allow choice of a 
+ * vibration pattern
+ */
+
 #include <pebble.h>
-
-#define LEVEL_1_ITEMS 4
-#define LEVEL_2_ITEMS 3
-
-typedef struct {
-  int type;
-} Context;
 
 typedef enum {
   VibrationTypeShort,
@@ -16,12 +15,15 @@ typedef enum {
   VibrationTypeCustomLong
 } VibrationType;
 
+typedef struct {
+  VibrationType type;
+} Context;
+
 static Window *s_main_window;
 static TextLayer *s_label_layer;
 static ActionBarLayer *s_action_bar;
 
 static GBitmap *s_ellipsis_bitmap;
-static int s_current_type;
 
 static ActionMenu *s_action_menu;
 static ActionMenuLevel *s_root_level, *s_custom_level;
@@ -30,18 +32,18 @@ static ActionMenuLevel *s_root_level, *s_custom_level;
 
 static void action_performed_callback(ActionMenu *action_menu, const ActionMenuItem *action, void *context) {
   // An action was selected, determine which one
-  s_current_type = *(int*)action_menu_item_get_action_data(action);
+  VibrationType type = *(VibrationType*)action_menu_item_get_action_data(action);
 
   // Play this vibration
   static uint32_t segments[5];
-  switch(s_current_type) {
+  switch(type) {
     case VibrationTypeShort:  vibes_short_pulse();  break;
     case VibrationTypeLong:   vibes_long_pulse();   break;
     case VibrationTypeDouble: vibes_double_pulse(); break;
     default:
       // Create the patten
       for(int i = 0; i < 5; i++) {
-        switch(s_current_type) {
+        switch(type) {
           case VibrationTypeCustomShort:  segments[i] = i * 100; break;
           case VibrationTypeCustomMedium: segments[i] = i * 200; break;
           case VibrationTypeCustomLong:   segments[i] = i * 300; break;
@@ -60,9 +62,8 @@ static void action_performed_callback(ActionMenu *action_menu, const ActionMenuI
 }
 
 static void init_action_menu() {
-  // Create the root level and secondary custom patterns level
-  s_root_level = action_menu_level_create(LEVEL_1_ITEMS);
-  s_custom_level = action_menu_level_create(LEVEL_2_ITEMS);
+  // Create the root level
+  s_root_level = action_menu_level_create(4);
 
   // Set up the actions for this level, using action context to pass types
   action_menu_level_add_action(s_root_level, "Short", action_performed_callback, 
@@ -72,8 +73,11 @@ static void init_action_menu() {
   action_menu_level_add_action(s_root_level, "Double", action_performed_callback, 
                                &(Context){.type=VibrationTypeDouble});
   
-  // Set up the secondary level's actions
+  // Create and set up the secondary level, adding it as a child to the root one
+  s_custom_level = action_menu_level_create(3);
   action_menu_level_add_child(s_root_level, s_custom_level, "Custom Pattern");
+
+  // Set up the secondary actions
   action_menu_level_add_action(s_custom_level, "Custom Fast", action_performed_callback, 
                                &(Context){.type=VibrationTypeCustomShort});
   action_menu_level_add_action(s_custom_level, "Custom Medium", action_performed_callback, 
@@ -116,13 +120,17 @@ static void window_load(Window *window) {
   action_bar_layer_set_icon(s_action_bar, BUTTON_ID_SELECT, s_ellipsis_bitmap);
   action_bar_layer_add_to_window(s_action_bar, window);
 
-  s_label_layer = text_layer_create(GRect(0, 0, bounds.size.w - ACTION_BAR_WIDTH, bounds.size.h));
+  s_label_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, bounds.size.w - ACTION_BAR_WIDTH, bounds.size.h));
   text_layer_set_text(s_label_layer, "Choose a vibration pattern from the Action Menu.");
   text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text_color(s_label_layer, GColorBlack);
   text_layer_set_background_color(s_label_layer, GColorClear);
   text_layer_set_text_alignment(s_label_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
+
+#if defined(PBL_ROUND)
+  text_layer_enable_screen_text_flow_and_paging(s_label_layer, 3);
+#endif
 }
 
 static void window_unload(Window *window) {
